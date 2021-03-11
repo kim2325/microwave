5c0ee436b6d5e1fc13f30829d8f38dfeb54b59fc
kim2325 <46657671+kim2325@users.noreply.github.com>
2021-03-11 16:12:50 -0500

    Add files via upload

---
+++ index.aspx
@@ -0,0 +1,179 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="project01.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+    	#container {
+    	    width: 700px;
+            height: 700px;
+            border-radius: 50px;
+            background-color: bisque;
+            position: absolute;
+			top: 10px;
+			left: 290px;
+		}
+
+    	#LabelBackground {
+    		width: 700px;
+    		height: 90px;
+    		position: relative;
+    	}
+
+    	#Label1 {
+    	    position: absolute;
+            width: 430px;
+            font-size: 32pt;
+			top: 20px;
+			left: 155px;
+		}
+
+    	#display {
+    	    position: absolute;
+            width: 275px;
+            height: 60px;
+			top: 90px;
+			left: 195px;
+			right: 222px;
+			margin-top: 0px;
+            text-align: center;
+        }
+        
+    	#Btn0 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 515px;
+			left: 290px;
+		}
+
+    	#Btn1 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 170px;
+			left: 170px;
+            right: 430px;
+        }
+        
+    	#Btn2 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 170px;
+			left: 290px;
+		}
+
+    	#Btn3 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 170px;
+			left: 410px;
+		}
+        
+    	#Btn4 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 285px;
+			left: 170px;
+		}
+        
+    	#Btn5 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 285px;
+			left: 290px;
+		}
+        
+    	#Btn6 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 285px;
+			left: 410px;
+		}
+        
+    	#Btn7 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 400px;
+			left: 170px;
+		}
+        
+    	#Btn8 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 400px;
+			left: 290px;
+		}
+        
+    	#Btn9 {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 400px;
+			left: 410px;
+		}
+        
+    	#start {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 515px;
+			left: 410px;
+		}
+        
+    	#clear {
+    	    position: absolute;
+            width: 100px;
+            height: 100px;
+            font-size: 24pt;
+			top: 515px;
+			left: 170px;
+		}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="container">
+            <div id="LabelBackground">
+				<asp:Label ID="Label1" runat="server" Text="Microwave Interface"></asp:Label>
+            </div> <!-- LabelBackground -->
+
+			<asp:TextBox ID="display" runat="server" BorderColor ="Black" BorderStyle="Solid" Font-Bold="False" Font-Size="40pt" MaxLength="6" OnTextChanged="display_TextChanged">00:00</asp:TextBox>
+
+	        <asp:Button ID="Btn0" runat="server" Text="0" OnClick="Btn0_Click" />
+            <asp:Button ID="Btn1" runat="server" Text="1" OnClick="Btn1_Click" />
+            <asp:Button ID="Btn2" runat="server" Text="2" OnClick="Btn2_Click" />
+            <asp:Button ID="Btn3" runat="server" Text="3" OnClick="Btn3_Click" />
+            <asp:Button ID="Btn4" runat="server" Text="4" OnClick="Btn4_Click" />
+            <asp:Button ID="Btn5" runat="server" Text="5" OnClick="Btn5_Click" />
+            <asp:Button ID="Btn6" runat="server" Text="6" OnClick="Btn6_Click" />
+            <asp:Button ID="Btn7" runat="server" Text="7" OnClick="Btn7_Click" />
+            <asp:Button ID="Btn8" runat="server" Text="8" OnClick="Btn8_Click" />
+            <asp:Button ID="Btn9" runat="server" Text="9" OnClick="Btn9_Click" />
+            <asp:Button ID="start" runat="server" Text="start" />
+            <asp:Button ID="clear" runat="server" Text="clear" OnClick="clear_Click" />
+
+        </div> <!-- container -->
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace project01
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private List<double> numbers;
+        private List<Operator> ops;
+        private string equation;
+        public Calculator()
+        {
+            InitializeComponent();
+            this.numbers = new List<double>();
+            this.ops = new List<Operator>();
+            this.equation = "";
+        }
+        private void UpdateTextBox(string str, TextBox tb)
+        {
+            tb.Text = str;
+        }
+        private double ConvertToDouble(string str)
+        {
+            return Double.Parse(str);
+        }
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+            this.equation += "0";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            this.equation += "1";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            this.equation += "2";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            this.equation += "3";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            this.equation += "4";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            this.equation += "5";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+            this.equation += "6";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+            this.equation += "7";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+            this.equation += "8";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+            this.equation += "9";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_plus_Click(object sender, EventArgs e)
+        {
+            this.numbers.Add(ConvertToDouble(this.equation));
+            this.ops.Add(new Operator('+'));
+            this.equation = "";
+        }
+
+        private void btn_minus_Click(object sender, EventArgs e)
+        {
+            this.numbers.Add(ConvertToDouble(this.equation));
+            this.ops.Add(new Operator('-'));
+            this.equation = "";
+        }
+
+        private void btn_multiply_Click(object sender, EventArgs e)
+        {
+            this.numbers.Add(ConvertToDouble(this.equation));
+            this.ops.Add(new Operator('*'));
+            this.equation = "";
+        }
+
+        private void btn_divide_Click(object sender, EventArgs e)
+        {
+            this.numbers.Add(ConvertToDouble(this.equation));
+            this.ops.Add(new Operator('/'));
+            this.equation = "";
+        }
+
+        private void btn_decimal_Click(object sender, EventArgs e)
+        {
+            this.equation += ".";
+            UpdateTextBox(this.equation, this.tb_equation);
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            this.equation = "";
+            UpdateTextBox(this.equation, this.tb_equation);
+            this.numbers.Clear();
+            this.ops.Clear();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_equals_Click(object sender, EventArgs e)
+        {
+            this.numbers.Add(ConvertToDouble(this.equation));
+            this.ops.Add(new Operator('='));
+            this.equation = this.solve();
+            this.UpdateTextBox(this.equation, this.tb_equation);
+        }
+        private string solve()
+        {
+            double before = numbers[0];
+            for (int i = 1; i < this.numbers.Count; i++)
+            {
+                before = ops[i - 1].apply(before, numbers[i]);
+            }
+            return before.ToString();
+        }
+
+
+        //protected void StoreDisplay(string fromDisplay)
+        //{
+        //    Session["displayedValue"] = fromDisplay.ToString();
+        //}
+
+        //protected void Btn0_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn0.Text.ToString();
+        //    StoreDisplay(Btn0.Text.ToString());
+        //}
+
+        //protected void Btn1_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn1.Text.ToString();
+        //    StoreDisplay(Btn1.Text.ToString());
+        //}
+
+        //protected void Btn2_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn2.Text.ToString();
+        //    StoreDisplay(Btn2.Text.ToString());
+        //}
+
+        //protected void Btn3_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn3.Text.ToString();
+        //    StoreDisplay(Btn3.Text.ToString());
+        //}
+
+        //protected void Btn4_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn4.Text.ToString();
+        //    StoreDisplay(Btn4.Text.ToString());
+        //}
+
+        //protected void Btn5_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn5.Text.ToString();
+        //    StoreDisplay(Btn5.Text.ToString());
+        //}
+
+        //protected void Btn6_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn6.Text.ToString();
+        //    StoreDisplay(Btn6.Text.ToString());
+        //}
+
+        //protected void Btn7_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn7.Text.ToString();
+        //    StoreDisplay(Btn7.Text.ToString());
+        //}
+
+        //protected void Btn8_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn8.Text.ToString();
+        //    StoreDisplay(Btn8.Text.ToString());
+        //}
+
+        //protected void Btn9_Click(object sender, EventArgs e)
+        //{
+        //    display.Text = Btn9.Text.ToString();
+        //    StoreDisplay(Btn9.Text.ToString());
+        //}
+
+        protected void clear_Click(object sender, EventArgs e)
+        {
+            display.Text = "";
+            
+        }
+    }
+}
